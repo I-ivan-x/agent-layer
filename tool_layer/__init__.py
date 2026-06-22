e44b2b514784661c91466c010c8b716386827241
fionaxi <1393367667@qq.com>
2026-06-22 16:58:41 +0800

    test: add local tool layer stub for real retrieval smoke test

---
+++ tool_layer/__init__.py
@@ -0,0 +1,8 @@
+from tool_layer.search_tool import SearchTool
+from tool_layer.search_tool import RetrievalError, RetrievalParameterError
+
+__all__ = [
+    "SearchTool",
+    "RetrievalError",
+    "RetrievalParameterError",
+]
